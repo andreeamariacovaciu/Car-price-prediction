95f95ff3d783d0551cc442f42d93699b9a324262
andreeamariacovaciu <86802852+andreeamariacovaciu@users.noreply.github.com>
2022-02-16 09:29:16 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,5 @@
 # Importing the libraries
 
-#import numpy as np
-#import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
@@ -12,18 +10,25 @@ X = df.drop(['current price','v.id'], axis=1)
 y = df['current price']
 
 
-#Splitting Training and Test Set
 #Since we have a very small dataset, we will train our model with all availabe data.
-
 from sklearn.linear_model import LinearRegression
 lr = LinearRegression()
 
 #Fitting model with trainig data
 lr.fit(X, y)
 
+#Finding the most important 6 features
+coef=lr.coef_
+coefser=pd.Series(coef)
+important_features=coefser.abs().nlargest(6)
+df_important_features=X.iloc[:,[a for a in important_features.index]]
+important_features_name=df_important_features.columns
+
+lr1=LinearRegression()
+lr1.fit(df_important_features,y)
+
 # Saving model to disk
-pickle.dump(lr, open('model.pkl','wb'))
+pickle.dump(lr1, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-#print(model.predict([[2, 9, 6]]))
